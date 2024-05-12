f745c3e7ab73d7bb17f875ae2441035c3295228a
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-05-12 10:43:12 +0530

    canPlaceFlowers

---
+++ CanPlaceFlowers.java
@@ -0,0 +1,24 @@
+public class CanPlaceFlowers {
+    
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0)
+          return true;
+    
+        for (int i = 0; i < flowerbed.length; ++i)
+          if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
+              (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+            flowerbed[i] = 1;
+            if (--n == 0)
+              return true;
+          }
+    
+        return false;
+      }
+
+      public static void main(String[] args) {
+         int [] flowerbed={1,0,0,0,1};
+         int n=1;
+        boolean result =canPlaceFlowers(flowerbed,n);
+        System.out.println(result);
+      }
+}
