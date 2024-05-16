d4ac4611d9414292e0e179d84da5807e48df83be
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-05-17 00:00:40 +0530

    MergeSortedArray

--- DuplicateZeros.java
+++ DuplicateZeros.java
@@ -3,20 +3,26 @@ public class DuplicateZeros {
 
     public static void duplicateZeros(int[] arr) {
         int n = arr.length;
-
-        int i = 0;
-
-        while (i < n) {
+        //int i = 0;
+        // {1, 0, 2, 3, 0, 4, 5};
+        for (int i = arr.length - 1; i >= 0; i--) {
             if (arr[i] == 0) {
-                for (int j = n - 1; j > i; i--) {
-                    arr[j] = arr[j - 1];
+                for (int k = arr.length - 2; k >= i; k--) {
+                    arr[k + 1] = arr[k];
                 }
-                if (i + 1 < n) {
-                    arr[i + 1] = 0;
-                }
-                i += 2;
             }
         }
+        // while (i < n) {
+        //     if (arr[i] == 0) {
+        //         for (int j = n - 1; j > i; i--) {
+        //             arr[j] = arr[j - 1];
+        //         }
+        //         if (i + 1 < n) {
+        //             arr[i + 1] = 0;
+        //         }
+        //         i += 2;
+        //     }
+        // }
 
         for (int k = 0; k < arr.length; k++) {
             System.out.print(arr[k] + " ");
