60c115c5daecfde17c94bae6474511adf73359e5
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-05-25 00:12:21 +0530

    threeSumClose

--- threeSumClosest.java
+++ threeSumClosest.java
@@ -6,11 +6,26 @@ public class threeSumClosest {
     public static int threeSumCloseste(int[] nums, int target) {
         int n = nums.length;
         Arrays.sort(nums);
-        for (int idx = 0; idx < nums.length; idx++) {
-
+        int closestSum = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int currentSum = nums[i] + nums[j] + nums[k];
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+                if (currentSum < target) {
+                    j++;  // Increase the sum by moving the left pointer to the right
+                } else if (currentSum > target) {
+                    k--;  // Decrease the sum by moving the right pointer to the left
+                } else {
+                    return currentSum;  // If the exact target is found, return immediately
+                }
+            }
         }
 
-        return 0;
+        return closestSum;
     }
 
     public static void main(String[] args) {
