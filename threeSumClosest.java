71694ded7ee7ba7faa222c04b002ce13faf65d31
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-05-24 23:52:41 +0530

    ThresumClosest

---
+++ threeSumClosest.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class threeSumClosest {
+
+    public static int threeSumCloseste(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int idx = 0; idx < nums.length; idx++) {
+
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+        int result = threeSumCloseste(nums, target);
+        System.out.print("Result " + result);
+    }
+}
