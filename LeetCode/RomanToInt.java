e34d9cd739c1e6960122bbbba5980c3a9539766c
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-06-06 23:00:21 +0530

    Roman to int

---
+++ LeetCode/RomanToInt.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.*;
+
+public class RomanToInt {
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            int currentvalue = romanValues.get(currentChar);
+
+            if (currentvalue < prevValue) {
+                total -= currentvalue;
+            } else {
+                total += currentvalue;
+            }
+            currentvalue = prevValue;
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("III"));
+    }
+}
