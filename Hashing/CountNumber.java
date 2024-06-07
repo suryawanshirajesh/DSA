6e8060e79d31b983ff0cf5f3adf012eb15463f3b
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-06-07 07:01:52 +0530

    Count Number

---
+++ Hashing/CountNumber.java
@@ -0,0 +1,26 @@
+
+public class CountNumber {
+
+    public static void frequencyCount(int arr[], int N, int P) {
+
+        for (int i = 0; i < N; i++) {
+            int count = 0;
+            for (int j = 1; j < N; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            arr[i] = count;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 4, 6, 4, 6, 1};
+        int N = arr.length;
+        int P = 1;
+        frequencyCount(arr, N, P);
+    }
+}
