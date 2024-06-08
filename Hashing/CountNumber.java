f58e2fb46157eaa920d8cb1e76e115013723a0e3
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-06-08 19:22:57 +0530

    Done

--- Hashing/CountNumber.java
+++ Hashing/CountNumber.java
@@ -5,33 +5,37 @@ public class CountNumber {
 
     public static void Count(int arr[]) {
         Arrays.sort(arr);
+        int count = 1;
         for (int i = 1; i < arr.length; i++) {
-            int count = 1;
-            if (arr[1 - i] == arr[i]) {
+
+            if (arr[i] == arr[i - 1]) {
                 count++;
             } else {
-                System.out.print(arr[1 - i] + " : " + count);
+                System.out.println(arr[i - 1] + " : " + count);
                 count = 1;
             }
         }
+        System.out.println(arr[arr.length - 1] + " : " + count);
     }
-    // public static void frequencyCount(int arr[], int N, int P) {
-    //     int count=0;
-    //     for (int i = 0; i < N; i++) {
-    //         if(arr[i]!=arr[i-1]){
-    //             count++;
-    //         }
-    //     }
-    //     for (int i : arr) {
-    //         System.out.print(i + " ");
-    //     }
-    // }
+
+    public static void frequencyCount(int arr[], int N) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            if (hash.containsKey(arr[i])) {
+                hash.put(arr[i], hash.get(arr[i]) + 1);
+            } else {
+                hash.put(arr[i], 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : hash.entrySet()) {
+            System.out.println(entry.getKey() + " :" + entry.getValue());
+        }
+    }
 
     public static void main(String[] args) {
         int arr[] = {1, 4, 4, 6, 4, 6, 1};
         int N = arr.length;
-        int P = 1;
-        // frequencyCount(arr, N, P);
-        Count(arr);
+        frequencyCount(arr, N);
+        // Count(arr);
     }
 }
