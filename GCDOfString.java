f2011765bfe8763f927e2f36b4e90ea1ae299058
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-06-15 18:18:57 +0530

    GCDOC of String

--- GCDOfString.java
+++ GCDOfString.java
@@ -3,25 +3,28 @@
 Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.*/
 
 public class GCDOfString {
-    public static  String gcdOfStrings(String str1, String str2) {
-      if(!(str1+str2).equals(str2+str1)){
-        return  "";
-      }
-      int gcdresult = gcd(str1.length(),str2.length());
-      return  str1.substring(0,gcdresult);
-    }
-      private static int gcd(int a,int b){
-        while(b!=0){
-            int temp=b;
-            b=a%b;
-            a=temp;
+
+    public static String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
         }
-        return  a;
-     }
+        int gcdresult = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdresult);
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
-        String str1="ABCABC";
-        String str2="ABC";
-       String name= gcdOfStrings(str1,str2);
-       System.out.println(name);
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+        String name = gcdOfStrings(str1, str2);
+        System.out.println(name);
     }
 }
