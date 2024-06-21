836f1ba99ba6e8999c31b72112b4ed8d7146b1be
Rajesh Suryawanshi <rsuryawanshi@c3itsolutions.com>
2024-06-21 09:23:18 +0530

    Done

--- InsertionSort/Insetionsortwithrecursion.java
+++ InsertionSort/Insetionsortwithrecursion.java
@@ -1,23 +1,41 @@
 package InsertionSort;
 
 public class Insetionsortwithrecursion {
-    
-    public static void Insertionsort(int [] arr){
-      for(int i=0;i<arr.length;i++){
-        int j=i;
-        while (j>0&&arr[j]>arr[j-1]) {
-            int temp=arr[j];
-            arr[j]=arr[j-1];
-            arr[j-1]=temp;
-            j--;
-        }
-      }
+  public static void InsertionSortrecursion1(int[] arr, int n) {
+    if (n>arr.length-1)
+      return;
+    int j = n;
+    while (j > 0 && arr[j] > arr[j - 1]) {
+      int temp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = temp;
+      j--;
     }
-    public static void main(String[] args) {
-        int[] arr = {2, 44, 23, 39, 68, 15, 12};
-        Insertionsort(arr);
-        for (int i : arr) {
-            System.out.print(i+" ");
-        }
+    InsertionSortrecursion1(arr, n+1);
+  }
+
+  public static void InsertionSortrecursion(int [] arr,int n){
+   if(n<=0){
+    return;
+   }
+   InsertionSortrecursion(arr, n-1);
+   int j=n;
+   while (j > 0 && arr[j] > arr[j - 1]) {
+    int temp = arr[j];
+    arr[j] = arr[j - 1];
+    arr[j - 1] = temp;
+    j--;
+  }
+  }
+
+
+  public static void main(String[] args) {
+    int[] arr = { 2, 44, 23, 39, 68, 15, 12 };
+    int n=arr.length;
+    //Insertionsort(arr);
+    InsertionSortrecursion(arr,n-1);
+    for (int i : arr) {
+      System.out.print(i + " ");
     }
+  }
 }
