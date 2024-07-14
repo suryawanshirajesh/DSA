11c5cf057b13d68b2b6f11fea2f14def60cfc1b1
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-07-14 23:16:02 +0530

    Largest element array

---
+++ ArrayProblem/Largestelement.java
@@ -0,0 +1,11 @@
+class Largestelement{
+    public static int largeelement(int [] num){ 
+        System.out.print("Hello");
+        return 0;
+    }
+
+    public static void main(String[] args){
+          int [] nums = {5,2,7,23,89,4};
+          largeelement(nums);
+    }
+}
