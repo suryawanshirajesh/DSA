d37b5f155abcfe5dd5bd42b2d1d245ed2775f7aa
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-07-15 23:55:43 +0530

    Using collection

--- ArrayProblem/Secountlargestelement.java
+++ ArrayProblem/Secountlargestelement.java
@@ -1,3 +1,5 @@
+//import java.util.List;
+import java.util.*;
 public class Secountlargestelement {
 
     public static int secoundelement(int [] arr){
@@ -15,8 +17,31 @@ public class Secountlargestelement {
      System.out.println("Secound Element "+Secoundelement);
     return 0;
     }
+
+    public static int print2largest(List<Integer> arr) {
+        int Largestelement=0;
+        int Secoundelement=0;
+        for (Integer integer : arr) {
+            if(Largestelement<integer){
+                Secoundelement=Largestelement;
+                Largestelement=integer;
+            }else if(Largestelement< Secoundelement &&Secoundelement<integer){
+                Secoundelement=integer;
+            }
+        }
+      return Secoundelement;
+    }
     public static void main(String[] args) {
          int [] nums = {5,2,7,23,89,4};
          secoundelement(nums);
+         List<Integer> list = new ArrayList<>();
+         list.add(2);
+         list.add(5);
+         list.add(7);
+         list.add(23);
+         list.add(89);
+         list.add(4);
+        int result= print2largest(list);
+        System.out.println("Result : "+result);
     }
 }
