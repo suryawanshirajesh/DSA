9ccbd267cad45b7e83e5dd5f6b3cc025cbae4737
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-07-17 00:02:05 +0530

    Check Array Sorted and Rotated

--- ArrayProblem/CheckArraySortedRotated.java
+++ ArrayProblem/CheckArraySortedRotated.java
@@ -20,9 +20,23 @@ public class CheckArraySortedRotated {
         // Otherwise, it is sorted and rotated
         return true;
     }
+
+    public static boolean check1(int [] nums){
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+             if(nums[i]>nums[(i+1)%n]){
+                count++;
+             }
+        }
+        if(count>1){
+            return false;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         int [] nums = {3,4,5,1,2};
-        boolean result=check(nums);
+        boolean result=check1(nums);
         System.out.println("Result : "+result);
 
     }
