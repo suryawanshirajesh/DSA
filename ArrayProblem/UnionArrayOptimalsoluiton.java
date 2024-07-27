6caad49e91fdf00f80320ebd86dd18a8ba160ef9
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-07-27 23:22:09 +0530

    Union Array

--- ArrayProblem/UnionArrayOptimalsoluiton.java
+++ ArrayProblem/UnionArrayOptimalsoluiton.java
@@ -1,42 +1,49 @@
 import java.util.ArrayList;
 
 public class UnionArrayOptimalsoluiton {
-    public static ArrayList<Integer> FindUnion(int [] arr1,int [] arr2){
-        ArrayList<Integer > Union=new ArrayList<>();
-        int n1=arr1.length;
-        int n2= arr2.length;
-        int i=0;
-        int j=0;
-        while(i<n1&&j<n2){
-            if(arr1[i]<=arr2[j]){
-                if(Union.size()==0||Union.get(Union.size()-1)!=arr1[i])
-                  Union.add(arr1[i]);
-                  i++;
-                
-            }else{
-                if(Union.size()==0||Union.get(Union.size()-1)!=arr2[j])
-                    Union.add(arr2[j]);
-                    j++;
-            }
-        }
-        while(i<n1){
-            if(Union.size()==0||Union.get(Union.size()-1)!=arr1[i]){
+    public static ArrayList<Integer> FindUnion(int [] arr1,int [] arr2,int n,int m){
+        ArrayList<Integer> Union = new ArrayList<>();
+    
+    int i = 0;
+    int j = 0;
+    while (i < n && j < m) {
+        if (arr1[i] < arr2[j]) {
+            if (Union.size() == 0 || Union.get(Union.size() - 1) != arr1[i]) {
                 Union.add(arr1[i]);
-                i++;
             }
-        }
-        while(j<n2){
-            if(Union.size()==0||Union.get(Union.size()-1)!=arr2[j]){
+            i++;
+        } else if (arr2[j] < arr1[i]) {
+            if (Union.size() == 0 || Union.get(Union.size() - 1) != arr2[j]) {
                 Union.add(arr2[j]);
-                j++;
             }
+            j++;
+        } else {
+            if (Union.size() == 0 || Union.get(Union.size() - 1) != arr1[i]) {
+                Union.add(arr1[i]);
+            }
+            i++;
+            j++;
         }
-        return Union;
+    }
+    while (i < n) {
+        if (Union.size() == 0 || Union.get(Union.size() - 1) != arr1[i]) {
+            Union.add(arr1[i]);
+        }
+        i++;
+    }
+    while (j < m) {
+        if (Union.size() == 0 || Union.get(Union.size() - 1) != arr2[j]) {
+            Union.add(arr2[j]);
+        }
+        j++;
+    }
+    return Union;
     }
     public static void main(String[] args) {
+        int n = 10, m = 7;
         int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   int arr2[] = {2, 3, 4, 4, 5, 11, 12};
-  ArrayList<Integer> Union = FindUnion(arr1, arr2);
+  ArrayList<Integer> Union = FindUnion(arr1, arr2,n,m);
   System.out.println("Union of arr1 and arr2 is ");
   for (int val: Union)
     System.out.print(val+" ");
