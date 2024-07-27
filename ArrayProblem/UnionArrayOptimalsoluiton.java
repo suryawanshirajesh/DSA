7922c9a75df189a4a46d44301eea31bb2a59db0f
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-07-27 21:20:07 +0530

    Union Array

---
+++ ArrayProblem/UnionArrayOptimalsoluiton.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class UnionArrayOptimalsoluiton {
+    public static ArrayList<Integer> Union(int [] arr1,int [] arr2){
+        int n1=arr1.length;
+        int n2= arr2.length;
+        
+        return null;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
