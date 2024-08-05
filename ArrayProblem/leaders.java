19c771edcf1e42daad84c0c5662681a7373ccba6
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-08-05 23:28:52 +0530

    Leaders

---
+++ ArrayProblem/leaders.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class leaders {
+
+    static ArrayList<Integer> findleader(int arr[]) {
+        int n= arr.length;
+        int max=Integer.MIN_VALUE;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]>max){
+                list.add(arr[i]);
+            }
+            max=Math.max(max, arr[i]);
+        }
+      return list;
+    }
+    public static void main(String[] args) {
+       int arr[] = {16,17,4,3,5,2};
+       List result = findleader(arr);
+       for (Object object : result) {
+          System.out.print(object+",");
+       }
+    }
+}
