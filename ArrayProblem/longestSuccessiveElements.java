cfe462cf60741146c09ad1b7f04a88f9daa4d46d
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2024-08-07 23:56:02 +0530

    Done

--- ArrayProblem/longestSuccessiveElements.java
+++ ArrayProblem/longestSuccessiveElements.java
@@ -4,16 +4,28 @@ import java.util.Set;
 public class longestSuccessiveElements {
     public static int longestSuccessiveElementcount(int arr[]){
         int n=arr.length;
-        int count=0;
-        int max=0;
+        int longest=1;
         Set<Integer> set = new HashSet<>();
         for(int i=0;i<n;i++){
             set.add(arr[i]);
         }
-     
+       
+        for(int it:set){
+
+            if(!set.contains(it-1)){
+                int count=1;
+                 int x=it;
+                 if(set.contains(x+1)){
+                    x=x+1;
+                    count = count+1;
+                 }
+                 longest = Math.max(count, longest);
+
+            }
+        }
             
         
-        return count;
+        return longest;
     }
     public static void main(String[] args) {
         int [] arr={100, 200, 1, 3, 2, 4};
