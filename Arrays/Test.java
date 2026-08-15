c5772ebf5dbb2c6cd17a7f12ed9ed0ccff0ba028
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2026-08-15 19:23:49 +0530

    Array Rotations

--- Arrays/Test.java
+++ Arrays/Test.java
@@ -1,4 +1,25 @@
 public class Test{
+      //Array Rotation
+       public static void reverse(int [] arr,int si,int ei){
+        while(si<ei){
+         int temp  = arr[si];
+         arr[si]=arr[ei];
+         arr[ei]=temp;
+         si++;
+         ei--;
+        }
+       }
+       public static void RotationArray(int [] arr,int k){
+         int N = arr.length;
+         k = k%N;
+        reverse(arr, 0, N-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, N-1);
+
+        for(int n:arr){
+            System.out.print(n+" ");
+        }
+       }
       //Good Pair
       public static int GoodPair(int [] arr,int B){
         for(int i=0;i<arr.length;i++){
@@ -11,9 +32,12 @@ public class Test{
         return  0;
       }
     public static void main(String [] args){
-        int [] arr ={1,2,3,4};
-        int B=6;
-        int result =GoodPair(arr, B);
-        System.out.print(result);
+           int [] arr = {1,2,3,4};
+           int k=2;
+           RotationArray(arr,k);
+        // int [] arr ={1,2,3,4};
+        // int B=6;
+        // int result =GoodPair(arr, B);
+        // System.out.print(result);
     }
 }
