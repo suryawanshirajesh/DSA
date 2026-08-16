186d1375eb321ca6a0752bd1fb582e25ba928e29
Rajesh Suryawanshi <rajeshsuryawanshi885586@gmail.com>
2026-08-16 13:24:43 +0530

    Range Sum Query

--- Arrays/Test.java
+++ Arrays/Test.java
@@ -1,4 +1,30 @@
 public class Test{
+
+         //Range Sum Query
+         public static int [] RangeSum(int []arr,int [][]query){
+            int Q = query.length;
+            int N = arr.length;
+            int [] ans = new int[Q];
+            int [] prefixsum = new int[N];
+            prefixsum[0]=arr[0];
+            for(int i=1;i<N;i++){
+               prefixsum[i]=prefixsum[i-1]+arr[i];
+            }
+
+            for(int i=0;i<Q;i++){
+                int left = query[i][0];
+                int right = query[i][1];
+                int sum=0;
+                if(left==0){
+                 sum = prefixsum[right];
+                }else{
+                    sum = prefixsum[right]-prefixsum[left-1];
+                }
+                ans[i]=sum;
+            }
+            return ans;
+         }
+
         // Count of elements
         public static int Countelements(int [] arr){
             int element=0;int max= arr[0];
@@ -61,9 +87,18 @@ public class Test{
         return  0;
       }
     public static void main(String [] args){
-           int [] arr = {1,2,3,4};
-           int k=2;
-           RotationArray(arr,k);
+          int [] arr ={1,2,3,4,5};
+          int [][] query = {{0,3},{2,3}};
+          int [] result = RangeSum(arr, query);
+          for(int n:result){
+            System.out.println(n);
+          }
+
+        //    int [] arr = {1,2,3,4};
+        //    int k=2;
+        //    RotationArray(arr,k);
+
+
         // int [] arr ={1,2,3,4};
         // int B=6;
         // int result =GoodPair(arr, B);
